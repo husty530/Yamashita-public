dc28052a15627bd2bb24cc09a5231bcaa08f9364
husty530 <husty530@gmail.com>
2021-02-14 02:32:09 +0900

    *

--- Library/Yamashita.Control/IFilter.cs
+++ Library/Yamashita.Control/IFilter.cs
@@ -3,8 +3,8 @@
     public interface IFilter
     {
         /// <summary>
-        /// フィルタ適用
-        /// ループの中でこれを呼び出すと観測値で更新→次期の予測を行う
+        /// フィルタ適用.
+        /// 観測値で更新→次期の予測を行う
         /// </summary>
         /// <param name="measurementVec">観測値のベクトル (m * 1)</param>
         /// <param name="controlVec">制御入力 (n * 1)</param>

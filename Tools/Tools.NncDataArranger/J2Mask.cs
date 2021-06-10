e860f9b55113221c67267489f39de7fef80861b6
husty530 <husty530@gmail.com>
2021-06-10 20:17:35 +0900

    *

--- Tools/Tools.NncDataArranger/J2Mask.cs
+++ Tools/Tools.NncDataArranger/J2Mask.cs
@@ -14,14 +14,18 @@ namespace Tools.NncDataArranger
             foreach (var file in files)
             {
                 if (Path.GetExtension(file) != ".json") continue;
-                var img = new Mat(size.Height, size.Width, MatType.CV_8U, 0);
-                foreach (var j in ((JObject)JsonConvert.DeserializeObject(File.ReadAllText(file)))["shapes"])
+                var json = (JObject)JsonConvert.DeserializeObject(File.ReadAllText(file));
+                var w = (int)json["imageWidth"];
+                var h = (int)json["imageHeight"];
+                var img = new Mat(h, w, MatType.CV_8U, 0);
+                foreach (var j in json["shapes"])
                 {
                     var poly = new List<Point>();
                     foreach (var p in j["points"])
                         poly.Add(new Point((int)p[0], (int)p[1]));
                     Cv2.FillPoly(img, new List<Point>[] { poly }, 255);
                 }
+                Cv2.Resize(img, img, size);
                 Cv2.ImWrite($"{outputDir}\\{Path.GetFileNameWithoutExtension(file)}.png", img);
             }
         }
